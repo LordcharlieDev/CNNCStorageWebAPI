9f33320db2eb43770614688496397a9663d55000
Oleksandr Demchuk <sashademchuk2000@gmail.com>
2022-10-30 15:47:40 +0200

    Create a DepartmentService and implemente an interface IDepartmentService.

---
+++ BusinessLogic/Services/DepartmentService.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLogic.DTOs;
+using BusinessLogic.Interfaces;
+using CNNCStorageDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class DepartmentService : IDepartmentService
+    {
+        public void Create(DepartmentDTO departmentDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(DepartmentDTO departmentDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
