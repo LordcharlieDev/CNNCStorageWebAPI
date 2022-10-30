6c2cb90ee3f4526dc7201ef44f6133f8de193efe
Oleksandr Demchuk <sashademchuk2000@gmail.com>
2022-10-30 15:26:00 +0200

    Add Department controller and constructor with database context.

---
+++ CNNCStorageWebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using CNNCStorageDB.Data;
+using CNNCStorageDB.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CNNCStorageWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly CNNCDbContext context;
+
+        public DepartmentController(CNNCDbContext context)
+        {
+            this.context = context;
+        }
+
+    }
+}
