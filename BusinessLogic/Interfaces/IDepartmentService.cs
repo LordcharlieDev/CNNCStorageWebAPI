d450ac21f537b2d84d5b78ac184794e4339acd09
Oleksandr Demchuk <sashademchuk2000@gmail.com>
2022-10-30 15:42:51 +0200

    Create an interface of department service and add GetAll, GetById, Create, Update, Delete methods.

---
+++ BusinessLogic/Interfaces/IDepartmentService.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogic.DTOs;
+using CNNCStorageDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IDepartmentService
+    {
+        IEnumerable<Department> GetAll();
+        Department GetById(int id);
+        void Create(DepartmentDTO departmentDTO);
+        void Update(DepartmentDTO departmentDTO);
+        void Delete(int id);
+    }
+}
